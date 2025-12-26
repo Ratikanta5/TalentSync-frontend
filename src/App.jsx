f6deb60939c9f0f4eec0444fcdbc6c7215b52e15
Ratikanta mohanty <136137940+Ratikanta5@users.noreply.github.com>
2025-12-26 15:07:06 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from '@clerk/clerk-react'
 import './App.css'
 
 function App() {
 
   return (
     <>
-     
+      <h1>Welcome to the TalentSync</h1>
+      <SignedOut>
+        <SignInButton mode='modal'>
+            <button>
+              Login
+            </button>
+          </SignInButton>
+      </SignedOut>
+
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </>
   )
 }
