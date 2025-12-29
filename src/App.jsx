de392d620506fce7f85287ff52278dd7023b0cd9
Ratikanta mohanty <136137940+Ratikanta5@users.noreply.github.com>
2025-12-29 18:41:54 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,28 @@ import { Routes, Route, Navigate } from 'react-router';
 import HomePage from './pages/HomePage';
 import ProblemsPage from './pages/ProblemsPage';
 import { useUser } from '@clerk/clerk-react';
-import {Toaster} from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
 import DashboardPage from './pages/DashboardPage';
+import ProblemPage from './pages/ProblemPage';
 
 function App() {
 
   const { isSignedIn, isLoaded } = useUser(); //useUser() is a clerk hook use to find the details of current login user.
 
   //this will get the rid of the flickering effect.
-  if(!isLoaded) return null;
+  if (!isLoaded) return null;
 
   return (
     <>
       <Routes>
 
-        <Route path='/' element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"}/>} />
-        <Route path='/dashboard' element={!isSignedIn ? <DashboardPage/> : <Navigate to={"/"}/>} />
+        <Route path='/' element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
+        <Route path='/dashboard' element={!isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
 
-        <Route path='/problems' element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
+        <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
+        <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
       </Routes>
-      <Toaster toastOptions={{duration:3000}}/>
+      <Toaster toastOptions={{ duration: 3000 }} />
     </>
   )
 }
